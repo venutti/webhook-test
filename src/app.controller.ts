1f6b469474a4737ac60295361c96837c50a774fc
venutti <lucasvenutti23@gmail.com>
2024-02-27 17:26:51 -0300

    Add dummy endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  get() {
+    console.log('GET');
+  }
+
+  @Post()
+  post(@Body() body) {
+    console.log('POST', body);
   }
 }
